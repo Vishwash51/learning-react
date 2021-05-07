ead717f781cd8c0fe10c140989886e0451046138
vishwash <vishwashshukla777@gmail.com>
2021-05-08 00:58:18 +0530

    commit

--- src/Hook/Form.js
+++ src/Hook/Form.js
@@ -1,5 +1,6 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState,useEffect, Fragment } from 'react';
 import {Form,Row,Col} from 'reactstrap'
+import './Form.css';
 
 function HookForm() {
     const now = new Date().toLocaleTimeString();
@@ -8,37 +9,77 @@ function HookForm() {
   const [Name, setName] = useState('');
   const [Mobile, setMobile] = useState('');
   const [Email, setEmail] = useState('');
+  let [darkMode,setDarkMode]=useState(false);
+  let [dark,setDark]=useState("");
 
   function updateTime(){
     const newTime = new Date().toLocaleTimeString();
-    setDate(newTime);
+    setDate(newTime)
   }
+
   function updateTimer(){
     setTimmer(timmer+1);
   }
   useEffect(() => {
     const timer=setInterval(() => {
         updateTime();
+    }, 1000);
+    return () => clearInterval(timer);
+  },[date]);
+
+  useEffect(() => {
+    const timer=setInterval(() => {
         updateTimer();
     }, 1000);
-    // Clear timeout if the component is unmounted
     return () => clearInterval(timer);
-  },[date],[timmer]);
+  },[timmer]);
   
+//----
+let checked, modeClass="";
+  useEffect(()=>{
+      debugger
+    darkMode ? setDark("dark-mode") : setDark("light-mode");
+    checked = darkMode ? "checked" : "unchecked";
+    modeClass = darkMode ? "dark-mode" : "light-mode";
+
+    document.getElementsByTagName('body')[0].className = modeClass
+   },[darkMode])
+   //---
+
   function reSetTimer(){
     setTimmer(timmer=0);
   }
 
   function handleSubmit(e) {
       alert('Name : '+Name+'\n Mobile : '+Mobile+'\n Email : '+Email)
-e.preventDefault();
+      e.preventDefault();
   } 
+  
+  function handleMode() {
+      debugger
+      setDarkMode(!darkMode);
+  }
+
   return (
-    <Form onSubmit={handleSubmit} style={{marginTop:'7%'}}>
+      <Fragment>
+          <div className="row">
+        <Col sm={11} className="text-right" style={{float:'right'}}>
+        <label className="checkbox">
+              <input 
+                type="checkbox" 
+                defaultChecked={checked} 
+                onChange={handleMode} />
+              {' '}Dark Mode
+            </label>
+        </Col>
+       
+    </div>
+    <h1 className="text-center">Using Hook</h1>
+    <Form onSubmit={handleSubmit} style={{marginTop:'7%'}} className={` ${dark}`}>
     <h1 style={{color:'white'}} className="text-center">{}</h1>
     <div className="row">
         <Col sm={11} className="text-right" style={{float:'right'}}>
-        date : {date} timmer : {timmer}
+        date : {date} timmer : {timmer}  {'   '} {` ${dark}`}
         </Col>
        
     </div>
@@ -74,15 +115,17 @@ e.preventDefault();
     <br />
     
     <div className="row mt-2">
-    <Col sm={4} className="offset-md-4">
+    <Col sm={2} className="offset-md-3">
             <input type="button" value="Reset Timer" onClick={reSetTimer} className="form-control btn btn-info" />
         </Col>
-        <Col sm={4} className="offset-md-4">
+        <Col sm={2} className="offset-md-1">
             <input type="submit" value="Save" className="form-control btn btn-info" />
         </Col>
        
     </div>
 </Form>
+<br />
+</Fragment>
    );
  }
  export default HookForm;
